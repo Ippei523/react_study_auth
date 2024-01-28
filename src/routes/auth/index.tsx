a87a5ff9469dada375eaff747ed28ec9555117c7
Ippei523 <ippei523@gmail.com>
2024-01-28 14:55:07 +0900

    スナックバーの実装

--- src/routes/auth/index.tsx
+++ src/routes/auth/index.tsx
@@ -1,5 +1,27 @@
+import { Alert, Snackbar } from "@mui/material";
+import { useAuth } from "../../context/authContext";
+
 export function Auth() {
+  const { missToken, setMissToken } = useAuth();
+  function handleClose() {
+    setMissToken(false);
+  }
+
   return (
-    <h1>Auth</h1>
+    <>
+      <h1>ログイン・ユーザー登録</h1>
+      {
+        missToken && (
+          <Snackbar
+            open={true}
+            onClose={handleClose}
+          >
+            <Alert onClose={handleClose} severity="error">
+              ログインまたはユーザー登録からやり直してください
+            </Alert>
+          </Snackbar>
+        )
+      }
+    </>
   )
 }
