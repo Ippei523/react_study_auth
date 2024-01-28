1131154ec4b34cfb3320d1b93e0de2c1e29caa3f
Ippei523 <ippei523@gmail.com>
2024-01-28 22:12:03 +0900

    認証部分を共通処理に変更

---
+++ src/components/authLayout.tsx
@@ -0,0 +1,77 @@
+import { Box, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+type AuthLayoutType = "login" | "signup";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  title: string;
+  toggleAuthButtonTitle: string;
+  toggleAuthSubTitle: string;
+  authLayoutType: AuthLayoutType;
+}
+
+export function AuthLayout({
+  children,
+  title,
+  toggleAuthButtonTitle,
+  toggleAuthSubTitle,
+  authLayoutType,
+}: LayoutProps) {
+  const router = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100vh",
+        gap: "10px",
+      }}
+    >
+      <Typography sx={{ fontSize: "30px", fontWeight: "bold" }}>
+        {title}
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "35%",
+          border: "1px solid blue",
+          padding: "60px 25px",
+          gap: "40px",
+          justifyContent: "space-between",
+        }}
+      >
+        {children}
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          marginTop: "20px",
+          justifyContent: "space-between",
+          width: "35%",
+        }}
+      >
+        <Typography>{toggleAuthSubTitle}</Typography>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => {
+            if (authLayoutType === "login") {
+              router("/auth/login");
+            } else {
+              router("/auth/signup");
+            }
+          }}
+        >
+          {toggleAuthButtonTitle}
+        </Button>
+      </Box>
+    </Box>
+  );
+}
