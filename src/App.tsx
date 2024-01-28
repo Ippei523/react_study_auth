3d9b628dc4c17c017012df2217ff40fe04dcf79e
Ippei523 <ippei523@gmail.com>
2024-01-28 14:37:48 +0900

    ルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Login } from './routes/auth/login';
+import { Signup } from './routes/auth/signup';
+import { Home } from './routes/home';
+import { Auth } from './routes/auth';
+import { useAuth } from './context/authContext';
 
 function App() {
+  const { token } = useAuth();
+
+  useEffect(() => {
+    console.log(token);
+  }, [token]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/auth" />} />
+      <Route index path="/auth" element={<Auth />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/signup" element={<Signup />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
