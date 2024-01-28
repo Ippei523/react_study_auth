4fa7f392fbf70972b43d23b8b2ccfa211bfcc87b
Ippei523 <ippei523@gmail.com>
2024-01-28 15:26:15 +0900

    ログイン画面作成途中

--- src/routes/auth/login/index.tsx
+++ src/routes/auth/login/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box, Snackbar } from "@mui/material";
+import { Alert, Box, Input, Snackbar, Typography } from "@mui/material";
 import { useAuth } from "../../../context/authContext";
 
 export function Login() {
@@ -8,8 +8,11 @@ export function Login() {
   }
   return (
     <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: "100%", height: "100vh", gap: "10px" }}>
-      <h1>Login</h1>
-
+      <Typography sx={{ fontSize: "30px", fontWeight: "bold" }}>Login</Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", width: "40%", gap: "20px", border: "1px solid blue", padding: "40px 20px" }}>
+        <Input type="email" placeholder="メールアドレスを入力してください" />
+        <Input type="password" placeholder="パスワードを入力してください" hidden />
+      </Box>
       {
         missToken && (
           <Snackbar
