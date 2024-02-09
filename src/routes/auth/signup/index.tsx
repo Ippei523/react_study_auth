edf799e934b1e6b62ee63a3b76648aafeb8ac40b
Ippei523 <ippei523@gmail.com>
2024-02-10 00:17:25 +0900

    新規登録画面の作成

--- src/routes/auth/signup/index.tsx
+++ src/routes/auth/signup/index.tsx
@@ -1,5 +1,36 @@
+import { Box, Button, Input } from "@mui/material";
+import { AuthLayout } from "../../../components/authLayout";
+
 export function Signup() {
   return (
-    <h1>Signup</h1>
-  )
+    <AuthLayout
+      title="新規登録"
+      toggleAuthButtonTitle="ログインへ"
+      toggleAuthSubTitle="すでにアカウントをお持ちの方"
+      authLayoutType="signup"
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "40px" }}>
+        <Input
+          sx={{ fontSize: "18px" }}
+          type="email"
+          placeholder="メールアドレスを入力してください"
+        />
+        <Input
+          sx={{ fontSize: "18px" }}
+          type="password"
+          placeholder="パスワードを入力してください"
+        />
+        <Input
+          sx={{ fontSize: "18px" }}
+          type="password"
+          placeholder="もう一度パスワードを入力してください"
+        />
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Button variant="contained" color="primary">
+          ユーザー登録
+        </Button>
+      </Box>
+    </AuthLayout>
+  );
 }
